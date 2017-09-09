9da0a35fbf850c98d9425b925a3b17d8327f7999
Rickie Jang <rickie.jang@dynamotechnologies.com>
2017-09-09 19:27:22 -0400

    about added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/teams/new' => 'teams#new', as: 'teams_new'
   resource :teams
 
+  get '/about/new' => 'about#new', as: 'about'
+  get '/roster/new' => 'roster#new', as: 'roster'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
